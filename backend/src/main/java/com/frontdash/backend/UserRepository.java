cf92db6628864c581f552dd1f60ff541daba55e9
mollieeh <mhamman@smu.edu>
2025-10-28 19:57:10 -0500

    adding tutorial files

---
+++ backend/src/main/java/com/frontdash/backend/UserRepository.java
@@ -0,0 +1,12 @@
+package com.frontdash.backend;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.frontdash.backend.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
